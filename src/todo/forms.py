675544071ef0ff43093aaf0097312ec531465dfb
Igor Becker <igor_nyu@hotmail.com>
2020-03-23 19:32:38 -0300

    template upgrade and style

--- src/todo/forms.py
+++ src/todo/forms.py
@@ -4,6 +4,8 @@ from .models import Todo
 class TodoForm(forms.ModelForm):
 	title = forms.CharField(max_length=200, required=True, label="Task", strip=True)
 
+	title.widget.attrs.update({'class': 'insert'})
+
 	class Meta:
 		model = Todo
 		fields = [
